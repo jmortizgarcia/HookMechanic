79d000e1b11d16799219e813ed846143fe2cec80
jmortizgarcia <jmortizgarcia@gmail.com>
2025-05-14 17:23:57 +0200

    Hook Ability Class
    Added depencies to plugin
    Created Base class for Hook ability

--- Source/HookMechanicRuntime/HookMechanicRuntime.Build.cs
+++ Source/HookMechanicRuntime/HookMechanicRuntime.Build.cs
@@ -4,50 +4,54 @@ using UnrealBuildTool;
 
 public class HookMechanicRuntime : ModuleRules
 {
-	public HookMechanicRuntime(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
+    public HookMechanicRuntime(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(
+            new string[] {
 				// ... add public include paths required here ...
 			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
+            );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
 				// ... add other private include paths required here ...
 			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
+            );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "GameplayAbilities",
+                "GameplayTags",
+                "GameplayTasks",
+                "LyraGame",
 				// ... add other public dependencies that you statically link with here ...
 			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
 				// ... add private dependencies that you statically link with here ...	
 			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
+            );
+
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
 				// ... add any modules that your module loads dynamically here ...
 			}
-			);
-	}
+            );
+    }
 }
---
+++ Source/HookMechanicRuntime/Public/LyraGameplayAbility_Hook.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "AbilitySystem/Abilities/LyraGameplayAbility.h"
+
+#include "LyraGameplayAbility_Hook.generated.h"
+
+/**
+ * ULyraGameplayAbility_Hook
+ *
+ *	Gameplay ability used for Hook into a wall.
+ */
+UCLASS()
+class HOOKMECHANICRUNTIME_API ULyraGameplayAbility_Hook : public ULyraGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
