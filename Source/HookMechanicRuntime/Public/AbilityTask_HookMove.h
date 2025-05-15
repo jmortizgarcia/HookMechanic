df476515261d1f45f64af5c23f9f8cae08047fb1
jmortizgarcia <jmortizgarcia@gmail.com>
2025-05-15 12:38:06 +0200

    Hook Move task base classes

---
+++ Source/HookMechanicRuntime/Public/AbilityTask_HookMove.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_HookMove.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOOKMECHANICRUNTIME_API UAbilityTask_HookMove : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
